90710ada565f3cdf44004513d163ddae23d227cd
Victor Xavier <vhvsites@hotmail.com>
2015-11-05 01:43:05 -0200

    HUD Changes
    Improvement in the health bar and adding effect to be hit...

--- Assets/Scripts/Jogo/Character.cs
+++ Assets/Scripts/Jogo/Character.cs
@@ -1,36 +1,52 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
-public class Character : MonoBehaviour {
-	private int health;
-	private float time = 5f;
+public class Character : MonoBehaviour{
+    private int health;
+    private float time = 5f;
+    private float flashSpeed = 3f;
+    private float lifeBarSpeed = 10f;
+    private Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
-	public Slider healthSlider;
+    public Slider healthSlider;
+    public Image damageImage;
 
-	public int Health {
-		get {
-			return health;
-		}
+    bool damaged;
 
-		set {
-			Debug.Log (value);
-			health = value;
-		}
-	}
+    public int Health{
+        get { return health; }
 
-	public void Bleed(int amount) {
-		health -= amount;
+        set{
+            Debug.Log(value);
+            health = value;
+        }
+    }
 
-		healthSlider.value = health/10f; // Vida atual/Vida maxima
+    public void Bleed(int amount){
+        health -= amount;
 
-		if (health <= 0) {
-			health = 0;
-			Die();
-		}
-	}
+        damaged = true;
+    }
 
-	virtual public void Die() {
-		Debug.Log("Character is dead");
-	}
+    virtual public void Die(){
+        Debug.Log("Character is dead");
+    }
+
+    void Update(){
+        if (damaged){
+            damageImage.color = flashColour;
+        }
+        else{
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        healthSlider.value = Mathf.Lerp(healthSlider.value, health, lifeBarSpeed * Time.deltaTime);
+
+        if (health <= 0){
+            health = 0;
+            Die();
+        }
+        damaged = false;
+    }
+
 }
