19491aa0992aeda7b3f7a55a36a611b4521d378f
vfernandes <vhvsites@hotmail.com>
2015-11-21 16:14:08 -0200

    Create intro scene
    Include intro.

---
+++ Assets/Scripts/Intro/SceneIntroController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneIntroController:Util{
+	
+	public string loadScene;
+	public float loadSceneDelay;
+	
+	void Start(){
+		LoadScene(loadScene, loadSceneDelay);
+	}
+
+}
---
+++ Assets/Scripts/Intro/fadeInElements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeInElements:Util {
+
+	public float timeToShowLabel;
+	public float timeToFadeInLabel;
+
+	void Start () {
+		gameObject.GetComponent<CanvasGroup> ().alpha = 0;
+		StartCoroutine(callLabel());
+	}
+
+	private IEnumerator callLabel(){
+		//Delay
+		yield return new WaitForSeconds(timeToShowLabel);
+		
+		StartCoroutine(fadeElement("in",gameObject,timeToFadeInLabel));
+	}
+
+}
