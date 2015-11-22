a9d7d2dc477ee441d7d850fea210a1da0cd435aa
Victor Xavier <vhvsites@hotmail.com>
2015-11-22 15:29:30 -0200

    Options Scene

---
+++ Assets/Scripts/Options/OnOffToggle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnOffToggle : MonoBehaviour {
+
+    public GameObject buttonOn;
+    public GameObject buttonOff;
+    public Sprite OnActive;
+    public Sprite OnInactive;
+    public Sprite OffActive;
+    public Sprite OffInactive;
+    public bool active = true;
+    
+    void Start () {
+        if (active) {
+            buttonOn.GetComponent<Image>().sprite = OnActive;
+            buttonOff.GetComponent<Image>().sprite = OffInactive;
+        }
+
+        buttonOff.GetComponent<Toggle>().active =
+        buttonOn.GetComponent<Toggle>().active = active;
+    }
+	
+	void Update () {
+        if (active) {
+            buttonOn.GetComponent<Image>().sprite = OnActive;
+            buttonOff.GetComponent<Image>().sprite = OffInactive;
+        }
+        else{
+            buttonOn.GetComponent<Image>().sprite = OnInactive;
+            buttonOff.GetComponent<Image>().sprite = OffActive;
+        }
+
+        buttonOff.GetComponent<Toggle>().active =
+        buttonOn.GetComponent<Toggle>().active = active;
+    }
+}
---
+++ Assets/Scripts/Options/Toggle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Toggle : MonoBehaviour {
+
+    public bool active;
+	
+    public void changeToOn () {
+        gameObject.GetComponentInParent<OnOffToggle>().active = true;
+    }
+
+    public void changeToOff(){
+        gameObject.GetComponentInParent<OnOffToggle>().active = false;
+    }
+}
