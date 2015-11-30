1e791d1c93d0c8f4e23f6343eafb1d506f35fb9c
shakadaran <gabriel_albino_5@hotmail.com>
2015-11-30 11:04:06 -0200

    Inimigo Atacando e movimentando

---
+++ Assets/Scripts/Jogo/AI_ENEMY.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+	[RequireComponent(typeof (Enemy))]
+	public class AI_ENEMY : MonoBehaviour {	
+
+		public bool ataque;
+		public Vector2 moveDirection;		
+		public TriggerChild shortcut;
+		private Enemy m_Character;        // Script Enemy.cs
+        private Animator m_Animator;         // Componente Animator    
+        private bool die;                // True -> die; False -> Live 
+
+
+
+        void Start () 
+		{
+			shortcut = gameObject.GetComponentInChildren<TriggerChild> ();		
+		}
+        private void Awake()
+        {
+            m_Character = GetComponent<Enemy>();
+            m_Animator = GetComponent<Animator>();
+            ataque = true;
+        }
+
+        private void FixedUpdate()
+        {
+            
+            if (!die)
+            {         
+              // Movimentation
+                if (!m_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack") && !m_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Hit"))
+                    {
+                    if (shortcut.state)
+                        {
+                            m_Character.Move(shortcut.moveDirection.normalized.x/4, shortcut.moveDirection.normalized.y/4);
+                        }
+                    else
+                        {
+                         m_Character.Move(0f, 0f);
+                         if (shortcut.isPlayerOnSight)
+                            {
+                                StartCoroutine("Attack", 1f);
+                            }
+                        }
+                ataque = true;
+                    }
+                
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        if (!ataque)
+        {
+            StopCoroutine("Attack");
+        }
+    }
+    private IEnumerator Attack  (float time)
+    {
+        yield return new WaitForSeconds(time);
+        m_Character.HandleAttack();
+        ataque = false; 
+    }
+
+    private void  takingHITS()
+    {
+        m_Character.HandleHit();
+    }
+    
+
+}
+
---
+++ Assets/Scripts/Jogo/TriggerChild.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerChild : MonoBehaviour {
+	public Vector2 moveDirection;
+	private GameObject player;    
+	public bool state;
+	public bool isPlayerOnSight;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		IsPlayerOnSight ();
+        
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject == player){
+			isPlayerOnSight = true;
+			state=true;
+            Debug.Log("entrou");
+		}
+	}
+	void OnTriggerExit2D(Collider2D col){
+		if (col.gameObject == player) {
+			isPlayerOnSight = false;
+            Debug.Log("saiu");
+        }
+	}
+
+	void OnTriggerStay2D(Collider2D col ) {
+		if(col.gameObject == player){
+			
+			if (Vector2.Distance(this.transform.position,player.transform.position)>3f){
+				state=true;
+                Debug.Log("state true");
+            }
+          
+            if (Vector2.Distance(this.transform.position, player.transform.position) <3f) {
+                Debug.Log("state false");
+				state=false;
+			}
+		}		
+	}
+	void IsPlayerOnSight(){
+		if (isPlayerOnSight){ 
+			
+			moveDirection = player.transform.position - transform.position;
+            
+		
+		}
+	}
+}
