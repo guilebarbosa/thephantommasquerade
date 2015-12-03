afb86374d440560ac86ca8d743e4fe073e545836
shakadaran <gabriel_albino_5@hotmail.com>
2015-12-03 00:20:44 -0200

    Final
    Tudo que eu fiz hj

--- Assets/Scripts/Jogo/EnemiesTrigger.cs
+++ Assets/Scripts/Jogo/EnemiesTrigger.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class EnemiesTrigger : MonoBehaviour {
     public GameObject EnemyPrefab;
-
-
-
+    
     
 
-	void OnTriggerEnter2D (Collider2D col) {
-		
-            Instantiate(EnemyPrefab, transform.position + (transform.right * 10), Quaternion.identity);
-            Debug.Log("Enemies spawned");
+    void OnTriggerEnter2D (Collider2D col) {
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 0)
+            {
+                
+                    Instantiate(EnemyPrefab, (transform.position + (transform.right*25) +(transform.up*3)), Quaternion.identity);
+            }
+            if (i == 1)
+            {
+                    Instantiate(EnemyPrefab, (transform.position + (transform.right * 25) + (transform.up * (-3))), Quaternion.identity);
+            }
+            if (i == 2)
+            {
+                    Instantiate(EnemyPrefab, (transform.position + (transform.right * (-25)) + (transform.up * (-3))), Quaternion.identity);
+            }
+            if (i == 3)
+            {
+                    Instantiate(EnemyPrefab, (transform.position + (transform.right * (-25)) + (transform.up * (+3))), Quaternion.identity);
+            }
+
+        }
         Destroy(this.gameObject);
-        
-	}
+    }
     
 }
